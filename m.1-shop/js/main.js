e6341af0353fd4a76fdc449bf869a6638fdcbada
vladsilav <vladmih1988@mail.ru>
2015-11-18 22:50:38 +0300

    перенос

---
+++ m.1-shop/js/main.js
@@ -0,0 +1,45 @@
+$(document).ready(function () {
+
+	$('.menue-btn').click(function(){
+		var menue = $('.main-nav');
+		if (menue.is(":hidden")) {
+			$('.menue-btn > span').text('скрыть меню');
+		} else {
+			$('.menue-btn > span').text('Меню');
+		}
+		menue.slideToggle(200);
+		$('body').toggleClass('overflow');
+	})
+
+	$('.filter-btn').click(function(){
+		$('.fiters').fadeIn(200);
+		$('body').addClass('overflow');
+	})
+	$('button.cancle').click(function(){
+		$('.fiters').fadeOut(200);
+		$('body').removeClass('overflow');
+	})
+
+	$('.filter-item span').click(function(){
+		$(this).toggleClass('flip').next().slideToggle(400);
+	})
+
+	$('#ToRecall').click(function() {
+		$('html, body').addClass('modal-show');
+		$('#RecallWin').fadeIn(300);
+		$('body').addClass('overflow');
+		return false;
+	})
+	$('#Findlow').click(function() {
+		$('html, body').addClass('modal-show');
+		$('#LowerWin').fadeIn(300);
+		$('body').addClass('overflow');
+		return false;
+	})
+
+	$('.btn-grey').click(function(){
+		$('html, body').removeClass('modal-show');
+		$('.overlay').fadeOut(300);
+		$('body').removeClass('overflow');
+	})
+})
