6aa5c41d3ed7436ee2a37ca67edde7023b55a856
vladsilav <vladmih1988@mail.ru>
2015-12-15 23:47:41 +0300

    правки, верстка шузов

--- shose/js/main.js
+++ shose/js/main.js
@@ -3,9 +3,14 @@ $(document).ready(function() {
 		mode: 'fade',
         pager: false,
         controls: false,
+        auto: true,
+        pause: 9000,
 		onSlideAfter: function (currentSlideNumber, totalSlideQty, currentSlideHtmlObject) {
             $('.active-slide').removeClass('active-slide');
             $('.banner-slider > li').eq(currentSlideHtmlObject).addClass('active-slide')
+            $('.banner-links a, .banner-pag > div').removeClass('active');
+            $('.banner-links a').eq(currentSlideHtmlObject).addClass('active');
+            $('.banner-pag > div').eq(currentSlideHtmlObject).addClass('active');
         },
         onSliderLoad: function () {
             $('.banner-slider > li').eq(0).addClass('active-slide')
@@ -19,5 +24,22 @@ $(document).ready(function() {
         $('.banner-pag > div').eq(currentLink).addClass('active');
         slider.goToSlide(currentLink);
         return false;
+    })
+
+    $('#SearchBtn').click(function(){
+        $('.page-wrap').addClass('show-search');
+        $('.search-layout').removeClass('hidden').addClass('search-in')
+            .on('animationend webkitanimationend oanimationend', function () {
+                $('.page-wrap').removeClass('show-search');
+                $(this).removeClass('search-in').removeClass('hidden')
+            });
+    })
+    $('#CloseSearch').click(function(){
+        $('.page-wrap').addClass('show-search');
+        $('.search-layout').addClass('search-out')
+            .on('animationend webkitanimationend oanimationend', function () {
+                $('.page-wrap').removeClass('show-search');
+                $(this).addClass('hidden').removeClass('search-out');
+            });
     })
 })
