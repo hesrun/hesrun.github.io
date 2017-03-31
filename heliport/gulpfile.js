a1a844d2bd5dda482360f68b99117a19f9fe4c32
hesrun <vladmih1988@mail.ru>
2017-03-31 14:18:49 +0300

    вертолеты

---
+++ heliport/gulpfile.js
@@ -0,0 +1,32 @@
+var gulp = require('gulp'),
+	sass = require('gulp-sass'),
+	browserSync = require('browser-sync').create(),
+	autoprefixer = require('gulp-autoprefixer'),
+	cssbeautify = require('gulp-cssbeautify');
+
+gulp.task('server', function() {
+	browserSync.init({
+        server: {
+            baseDir: "."
+        },
+		notify: false
+    });
+});
+gulp.task('style', function() {
+	gulp.src('./scss/**/*.scss')
+		.pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))
+		.pipe(autoprefixer({
+			browsers: ['last 3 versions'],
+			cascade: true
+		}))
+		.pipe(cssbeautify())
+		.pipe(gulp.dest('./css'));
+});
+
+gulp.task('watch', function () {
+	gulp.watch('./scss/*.scss', ['style']);
+	gulp.watch('./scss/*.scss', browserSync.reload);
+	gulp.watch('./*.html', browserSync.reload);
+});
+
+gulp.task('default', ['server', 'style', 'watch']);
