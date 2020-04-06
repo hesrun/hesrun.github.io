1c029e009b31f1a5f4cbd9cbda376c0c3f945c1f
hesrun <vladmih1988@mail.ru>
2020-04-06 15:23:43 +0200

    правки

--- CHRONOFUND/js/main.js
+++ CHRONOFUND/js/main.js
@@ -5,5 +5,20 @@ $('.c-btn.blue').click(function () {
      easing: 'easeInQuad',
      duration: 600
    });
+   anime({
+     targets: '.bottom-btn',
+     rotateX: ['90deg', '0'],
+     easing: 'easeInQuad',
+     duration: 200,
+     delay: 1000
+   });
    return false;
 })
+
+anime({
+  targets: '.bottom-btn',
+  rotateX: ['90deg', '0'],
+  easing: 'easeInQuad',
+  duration: 200,
+  delay: 1000
+});
