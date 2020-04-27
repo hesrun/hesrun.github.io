3f1f405ac0be8755912f7b941b5e91da2e27ed3e
hesrun <vladmih1988@mail.ru>
2020-04-27 17:16:31 +0200

    индекс

---
+++ 3-veka/js/main.js
@@ -0,0 +1,15 @@
+$('.main-slider .owl-carousel').owlCarousel({
+   loop:true,
+   margin:0,
+   nav:false,
+   dots:true,
+   items:1
+})
+
+$('.menu-carusel .owl-carousel').owlCarousel({
+   loop:true,
+   margin:0,
+   nav:true,
+   dots:false,
+   items:4
+})
