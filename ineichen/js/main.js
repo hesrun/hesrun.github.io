555206d1c87acb75b6a2ecdd2ebfa35d74979f1e
hesrun <vladmih1988@mail.ru>
2021-04-05 00:41:37 +0200

    апдейт

--- ineichen/js/main.js
+++ ineichen/js/main.js
@@ -3,7 +3,6 @@ var scTop = 0,
 
 $(window).scroll(function(){
     scTop = $(this).scrollTop();
-    console.log(scTop);
     if (scTop > 400) {
         header.addClass('white');
         if (scTop > 800) {
