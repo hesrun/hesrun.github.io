15f177cd68ae9cbf5a2356e7fbf3b54cdc068036
hesrun <vladmih1988@mail.ru>
2021-04-28 21:07:38 +0200

    правки

--- ineichen/js/main.js
+++ ineichen/js/main.js
@@ -3,14 +3,22 @@ var scTop = 0;
 
 $(window).scroll(function(){
     scTop = $(this).scrollTop();
-    if (scTop > 200) {
-        $('.main-header.clear').addClass('white');
-        if (scTop > 800) {
-            $('.main-header').addClass('mini');
-        } else {
-            $('.main-header').removeClass('mini');    
-        }
+    if (scTop > 400) {
+        $('.main-header').addClass('mini');
     } else {
-        $('.main-header.clear').removeClass('white');
+        $('.main-header').removeClass('mini');    
     }
+})
+
+$('.burger-btn').click(function(){
+    $('.side-menu').toggleClass('open');
+})
+
+$('#ShowSearch').click(function(){
+    $('#HeadSearch').fadeIn(300);
+    return false;
+})
+$('#HeadSearch .close').click(function(){
+    $('#HeadSearch').fadeOut(300);
+    return false;
 })
