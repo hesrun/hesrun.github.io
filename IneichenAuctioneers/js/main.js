9d6096b699ec984dbc96c6a87e2ae85e95778496
hesrun <vladmih1988@mail.ru>
2021-09-30 23:56:33 +0200

    auction-item.html

--- IneichenAuctioneers/js/main.js
+++ IneichenAuctioneers/js/main.js
@@ -14,4 +14,14 @@ $(document).ready(function() {
             once: true
         });
     })
+    $('.lot-item').each(function(){
+        AOS.init({
+            once: true
+        });
+    })
+
+    $('.lot-item__favorite').click(function() {
+        $(this).toggleClass('lot-item__favorite_active')
+        return false;
+    })
 });
