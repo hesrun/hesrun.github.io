5e61d433e44cbc5e30e51dff5b9158253c06fd0e
hesrun <vladmih1988@mail.ru>
2022-12-11 00:09:50 +0100

    правки

--- Independents/js/main.js
+++ Independents/js/main.js
@@ -1,3 +1,6 @@
+let ShowMore = 1;
+let CollapseHeight = $('.collapse-content').innerHeight();
+
 $('#openSearch').click(function(){
     $('.main-header__search').addClass('main-header__search_open');
     return false;
@@ -21,14 +24,34 @@ const mediaQuery = window.matchMedia('(max-width: 576px)')
 function handleTabletChange(e) {
 // Check if the media query is true
 if (e.matches) {
-    $('.main-header__nav').append( $('.main-header__tools > .button') );
-        console.log('Media Query Matched!')
+    $('.main-header__nav').append($('.main-header__tools') );
+    $('.collapse-content').css({
+        height: 300,
+        overflow: 'hidden',
+        transition: 'height 0.3s'
+    })
     } else {
-        $('.main-header__tools').append( $('.main-header__nav > .button') );
-        console.log('Media Query failed!')
+        $('.main-header__cols').append( $('.main-header__tools') );
+        $('.collapse-content').removeAttr('style');
     }
 }
 mediaQuery.addListener(handleTabletChange)
 // Initial check
 handleTabletChange(mediaQuery)
 
+
+
+$('.collapse-content-btn .button').click(function(){
+    $(this).toggleClass('active');
+    $('.collapse-content').toggleClass('open');
+    if (ShowMore == 1) {
+        $('.collapse-content').height(CollapseHeight);
+        $(this).text('Show less');
+        ShowMore = 0;
+    } else {
+        $('.collapse-content').height(300);
+        $(this).text('Show more');
+        ShowMore = 1;
+    }
+    return false;
+})
