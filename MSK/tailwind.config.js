83398ba747b1522d681b7b6211332f47894afab7
hesrun <vladmih1988@mail.ru>
2023-01-30 22:35:59 +0100

    updates new project

---
+++ MSK/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.html"],
+  theme: {
+    extend: {
+      colors: {
+        'brown': '#C28D3F',
+      },
+    },
+  },
+  plugins: [],
+}
