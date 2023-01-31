8865776e630467eea719db5ce46705dddd73a583
hesrun <vladmih1988@mail.ru>
2023-01-31 21:36:20 +0100

    updates

---
+++ MSK/js/main.js
@@ -0,0 +1,30 @@
+$('#menuOpen').click(function(){
+    $(this).toggleClass('active');
+    $('#mainNav').fadeToggle();
+})
+
+$('.c-select select').select2({
+    minimumResultsForSearch: -1,
+    width: '100%'
+});
+
+$('.c-field').each(function() {
+    var field = $(this);
+    var input = field.find('.c-field__input');
+    function checkInput() {
+        var valueLength = input.val().length;
+        console.log(valueLength);
+        if (valueLength > 0 ) {
+            field.addClass('active')
+        } else {
+            field.removeClass('active')
+        }
+    }
+    $('.c-field__input').blur(function() {
+        checkInput()
+    })
+    $('.c-field__input').focus(function() {
+        checkInput();
+        $(this).parents('.c-field').addClass('active');
+    })       
+});
