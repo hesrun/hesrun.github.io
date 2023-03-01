953435441ebc34422f94dcd0e4b002a066df915c
hesrun <vladmih1988@mail.ru>
2023-03-01 21:04:54 +0100

    updates

--- GellatoNew/js/main.js
+++ GellatoNew/js/main.js
@@ -8,13 +8,35 @@ $('.catalog-nav__btn').click(function(){
     $('.fa-solid ').toggleClass('fa-bars fa-xmark')
 })
 
+
+/* ------------------------------- mobile menu ------------------------------ */
 $('.main-header__lang-current').click(function(){
     $(this).toggleClass('active').next().toggle();
 }) 
 
+$('.header-mobile__lang-current').click(function(){
+    $(this).next().slideToggle(100);
+}) 
+
+$('.header-mobile__menu-open').click(function(){
+    $(this).toggleClass('active').find('.fa-solid ').toggleClass('fa-bars fa-xmark');
+    $('.header-mobile__nav').fadeToggle(300);
+    $('.header-mobile__bottom').toggleClass('active');
+})
+
+$('.has-sub').click(function(){
+    $(this).next().fadeIn();
+    return false;
+})
+$('.header-mobile__close-sub').click(function(){
+    $(this).parent().fadeOut();
+})
+
+/* ------------------------------- mobile menu end ------------------------------ */
+
 
 function Device(x) {
-    if (!x.matches) { // If media query matches
+    if (!x.matches) {
         $(window).scroll(function(){
             scTop = $(this).scrollTop();
             if (scTop > 300){
@@ -28,6 +50,17 @@ function Device(x) {
                 $('.main-header').removeClass('main-header_sticky');
                 $('.main-header__nav').prepend($('.catalog-nav') );
                 $('body').removeAttr('style');
+            }
+        })
+    } else {
+        console.log('gfgf');
+        $(window).scroll(function(){
+            scTop = $(this).scrollTop();
+            if (scTop > 65){
+                $('.header-mobile__bottom').addClass('sticky')
+            }
+            else {
+                $('.header-mobile__bottom').removeClass('sticky')
             }
         })
     }
